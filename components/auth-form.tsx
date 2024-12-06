b41b6388a24291feef880aec6fa2066d9c4df755
Space <michael.wlt@proton.me>
2024-12-06 22:00:18 +0100

    updated sing up page

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -15,45 +15,22 @@ export function AuthForm({
   defaultEmail?: string;
 }) {
   return (
-    <Form action={action} className="flex flex-col gap-4 px-4 sm:px-16">
-      <div className="flex flex-col gap-2">
-        <Label
-          htmlFor="email"
-          className="text-zinc-600 font-normal dark:text-zinc-400"
-        >
-          Email Address
-        </Label>
-
-        <Input
-          id="email"
-          name="email"
-          className="bg-muted text-md md:text-sm"
-          type="email"
-          placeholder="user@acme.com"
-          autoComplete="email"
-          required
-          autoFocus
-          defaultValue={defaultEmail}
-        />
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <Label
-          htmlFor="password"
-          className="text-zinc-600 font-normal dark:text-zinc-400"
-        >
-          Password
-        </Label>
-
-        <Input
-          id="password"
-          name="password"
-          className="bg-muted text-md md:text-sm"
-          type="password"
-          required
-        />
-      </div>
-
+    <Form action={action} className="flex flex-col gap-4 px-4">
+      <input
+        type="email"
+        name="email"
+        defaultValue={defaultEmail}
+        placeholder="your@email.com"
+        required
+        className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:border-gray-400 focus:outline-none"
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="never gonna give you up"
+        required
+        className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:border-gray-400 focus:outline-none"
+      />
       {children}
     </Form>
   );
