9db960431d9ecf1d515d7482dc15885fe0115da6
Space <michael.wlt@proton.me>
2024-12-11 20:05:54 +0100

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -13,6 +13,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api/turnstile|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api/turnstile|_next/static|_next/image|favicon.ico|fonts).*)',
   ],
 };
